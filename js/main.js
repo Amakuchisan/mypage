1bf48dd7b0a8b81f4562a9563fb92bff6699f205
Amakuchisan <s18t318@stu.kagawa-u.ac.jp>
2018-11-14 15:51:23 +0900

    varが混ざっていたのでletに変更，console.logが残っていたので削除

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 window.onload = function(){
     document.getElementById("name").addEventListener("click", function(){
-        var name = prompt("お名前は？");
-        console.log(name);
+        let name = prompt("お名前は？");
         document.getElementById("talk").textContent = name + "さん、ようこそ！";
     });
   }
